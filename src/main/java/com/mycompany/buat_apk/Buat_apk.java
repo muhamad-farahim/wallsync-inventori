8a07ab7a1bdddbb3b74238e473ced32980118319
njwamnda19 <najw4amanda@gmail.com>
2026-04-21 16:01:42 +0700

    komit pertama

---
+++ src/main/java/com/mycompany/buat_apk/Buat_apk.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.buat_apk;
+
+/**
+ *
+ * @author najwa amanda
+ */
+public class Buat_apk {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
